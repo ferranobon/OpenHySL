bfd8e8683c233fdd0f5ffe29cd3e44eca79315d6
Ferran Obón Santacana <ferran.obon@gmail.com>
2012-02-20 16:01:02 +0100

    Added support to run the Substructure without ADwin in the Custom protocol. It is a dummy substructure where fc = 0 and uc = u0c. It is activated in compile time through the TEST variable in CMake.

--- Effective_Stiffness_Formulation/Substructure/src/Substructure.c
+++ Effective_Stiffness_Formulation/Substructure/src/Substructure.c
@@ -8,3 +8,12 @@ void Init_Constants_Substructure( ConstSub *const Constants )
      (*Constants).DeltaT = 0.01;
      (*Constants).DeltaT_Sub = (*Constants).DeltaT/(float)(*Constants).Num_Sub;
 }
+
+void Simulate_Substructure( const float *const u0c, float *const uc, float *const fcprev, float *const fc, const int OrderC, const int NSub, const float Deltat_Sub )
+{
+
+     fc[0] = 0.0;
+     fcprev[0] = 0.0;
+
+     uc[0] = u0c[0];
+}
