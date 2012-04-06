6d80f3efee659ad273a215c691833a2103cacb82
Ferran Obón Santacana <ferran.obon@gmail.com>
2012-04-06 10:57:00 +0200

    The NSEP interface has been enhanced in order to take advantage of the ConstSub structure defined in Substructure.h. Also, the possibility to run purely numerical simulations without ADwin has been added through the use of
    #if SIMULATE_SUB_
     Simulate the substructure code
    #else
     ADwin Communication code
    #endif

--- Effective_Stiffness_Formulation/Substructure/src/NSEP_Dorka_FCM.c
+++ Effective_Stiffness_Formulation/Substructure/src/NSEP_Dorka_FCM.c
@@ -11,6 +11,7 @@
 #include "Send_Receive_Data.h" /* Send and receive data routines */
 #include "ErrorHandling.h"     /* Error handling routines */
 #include "RoutinesADwin.h"     /* Routines to communicate with ADwin */
+#include "Substructure.h"
 
 
 int main ( int argc, char **argv )
@@ -19,53 +20,57 @@ int main ( int argc, char **argv )
      int i, j;                                /* Counters */
      int Is_Not_Finished;
 
-     int OrderCouple = 1;
-     const int Num_Sub = 4;
-     const float DeltaT_Sub = 0.01/(float)Num_Sub;
+     ConstSub Cnst;
 
      float *Gc;
      float *u0c, *uc;
 
      float *fcprev, *fc;
-     float Send[3*OrderCouple], Recv[OrderCouple];
+     float *Send, *Recv;
 
      float *ADWIN_DATA;
 
-     Send[0] = 0.0;
-     Send[1] = 0.0;
-     Send[2] = 0.0;
-
-     Recv[0] = 0.0;
-
-     Gc = calloc( OrderCouple*OrderCouple, sizeof( float ) );
+     /* Dynamically allocate memory */
+     Gc = calloc( Cnst.Order_Couple*Cnst.Order_Couple, sizeof( float ) );
  
-     u0c = calloc( OrderCouple, sizeof( float ) );
-     uc = calloc( OrderCouple, sizeof( float ) );
+     u0c = calloc( Cnst.Order_Couple, sizeof( float ) );
+     uc = calloc( Cnst.Order_Couple, sizeof( float ) );
 
-     fcprev = calloc( OrderCouple, sizeof( float ) );
-     fc = calloc( OrderCouple, sizeof( float ) );
+     fcprev = calloc( Cnst.Order_Couple, sizeof( float ) );
+     fc = calloc( Cnst.Order_Couple, sizeof( float ) );
 
-     ADWIN_DATA = calloc( 4*4096*22, sizeof(float));
+     Send = calloc( 3*Cnst.Order_Couple, sizeof( float ) );
+     Recv = calloc( Cnst.Order_Couple, sizeof( float ) );
+
+#if SIMULATE_SUB_
+     /* Do nothing */
+#else
+     ADWIN_DATA = calloc( Cnst.Num_Sub*Cnst.Num_Steps*NUM_CHANNELS, sizeof( float ) );
+#endif
 
      /* Using NSEP */
      /* Open the Socket */
      printf( "Establishing connection with PNSE server.\n" );
-     Communicate_With_PNSE( 0, 0.0, Send, Recv, OrderCouple );
+     Communicate_With_PNSE( 0, 0.0, Send, Recv, Cnst.Order_Couple );
 
      /* Receive the matrix Gc from the CGM facility */
-     for ( i = 0; i < OrderCouple; i++ ){
-	  Communicate_With_PNSE( 3, 0.0, Send, Recv, OrderCouple*OrderCouple );
-	  for ( j = 0; j < OrderCouple; j++ ){
-	       Gc[i*OrderCouple + j] = Recv[j];
+     for ( i = 0; i < Cnst.Order_Couple; i++ ){
+	  Communicate_With_PNSE( 3, 0.0, Send, Recv, Cnst.Order_Couple*Cnst.Order_Couple );
+	  for ( j = 0; j < Cnst.Order_Couple; j++ ){
+	       Gc[i*Cnst.Order_Couple + j] = Recv[j];
 	  }
 	  /* This is in done so that the PNSE don't overtake the first step */
-	  Communicate_With_PNSE( 4, 0.0, Send, Recv, 3*OrderCouple );
+	  Communicate_With_PNSE( 4, 0.0, Send, Recv, 3*Cnst.Order_Couple );
      }
-     
+#if SIMULATE_SUB_  /* Run this without ADwin */
+     /* Do nothing */
+     printf("Simulating the substructure\n");
+#else
      /* Set Gc to ADwin */
-     ADWIN_SetGc( Gc, OrderCouple*OrderCouple );
+     ADWIN_SetGc( Gc, Cnst.Order_Couple*Cnst.Order_Couple );
+#endif
 
-     for( i = 0; i< OrderCouple*OrderCouple; i++ ){
+     for( i = 0; i< Cnst.Order_Couple*Cnst.Order_Couple; i++ ){
 	  printf("Gc = %e\t", Gc[i]);
      }
      printf("\n");
@@ -74,24 +79,28 @@ int main ( int argc, char **argv )
      while ( Is_Not_Finished ){
 
 	  /* Receive the displacement from the CGM facility */
-	  Communicate_With_PNSE( 3, 0.0, Send, Recv, OrderCouple );
-	  for ( j = 0; j < OrderCouple; j++ ){
+	  Communicate_With_PNSE( 3, 0.0, Send, Recv, Cnst.Order_Couple );
+	  for ( j = 0; j < Cnst.Order_Couple; j++ ){
 	       u0c[j] =  Recv[j];
 	  }
 
+#if SIMULATE_SUB_  /* Run this without ADwin */
+	  Simulate_Substructure( u0c, uc, fcprev, fc, Cnst.Order_Couple, Cnst.Num_Sub, Cnst.DeltaT_Sub );
+#else              /* Run using ADwin */
 	  /* Perform the substepping process */
-	  ADWIN_Substep( u0c, uc, fcprev, fc, OrderCouple, Num_Sub, DeltaT_Sub );
+	  ADWIN_Substep( u0c, uc, fcprev, fc, Cnst.Order_Couple, Cnst.Num_Sub, Cnst.DeltaT_Sub );
+#endif	  
 
 	  /* Compose the data to send */
-	  for (i = 0; i < OrderCouple; i++) {
+	  for (i = 0; i < Cnst.Order_Couple; i++) {
 	       Send[i] = uc[i];
-	       Send[i+OrderCouple] = fcprev[i];
-	       Send[i+2*OrderCouple] = fc[i];
+	       Send[i+Cnst.Order_Couple] = fcprev[i];
+	       Send[i+2*Cnst.Order_Couple] = fc[i];
 	  }
 	  /* Send the NSEP_CSIG package. WhatToDo = 4 */
-	  Communicate_With_PNSE( 4, 0.0, Send, Recv, 3*OrderCouple );
+	  Communicate_With_PNSE( 4, 0.0, Send, Recv, 3*Cnst.Order_Couple );
           /* Receive the state of the experiment. WhatToDo = 5 */
-	  Communicate_With_PNSE( 5, 0.0, Send, Recv, OrderCouple );
+	  Communicate_With_PNSE( 5, 0.0, Send, Recv, Cnst.Order_Couple );
 	  if ( Recv[0] == -9999.0 ){
 	       Is_Not_Finished = 0;
 	  }
@@ -100,10 +109,18 @@ int main ( int argc, char **argv )
      /* Say to PNSE server that the process has finished. WhatToDo = 6 */
      Communicate_With_PNSE( 6, 0.0, Send, Recv, 0 );
 
-     GetDataADwin( 4096, Num_Sub, ADWIN_DATA );
+#if SIMULATE_SUB_  /* Run this without ADwin */
+     printf("The simulatiovn has finished\n");
+#else
+     /* Get the Data from ADwin */
+     printf("Getting the data from ADwin...");
+     GetDataADwin( Cnst.Num_Steps, Cnst.Num_Sub, ADWIN_DATA );
+     printf(" DONE!\n");
 
-     /* Free the dinamically allocated memory */
      free( ADWIN_DATA );
+#endif
+
+     /* Free the dinamically allocated memory */     
 
      free( Gc );
 
@@ -112,6 +129,9 @@ int main ( int argc, char **argv )
 
      free( fcprev );
      free( fc );
+
+     free( Send );
+     free( Recv );
 
      return 0;
 }
