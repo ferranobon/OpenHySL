06cc8d7561c9a69bc0e54d681603c088f70c5a24
Ferran Obón Santacana <ferran.obon@gmail.com>
2012-05-18 08:36:51 +0200

    Initial implementation of the simulation of the TMD

--- Effective_Stiffness_Formulation/Substructure/include/Substructure.h
+++ Effective_Stiffness_Formulation/Substructure/include/Substructure.h
@@ -11,6 +11,21 @@ typedef struct{
      float DeltaT_Sub;   /* Time between substeps. */
 } ConstSub;
 
+typedef struct{
+     float Disp0, Vel0;
+     float Disp, Vel;
+     float Force_0, Force_1;
+     float u0c_old;      /* Backup of the displacement at the coupling node */
+     float v0c;          /* Velocity at the coupling node */
+
+     float A,B,C,D,E,F,G,H;  /* Several constants */
+} TMD_Sim;
+
+
 void Init_Constants_Substructure( ConstSub *const Constants );
+
+
+void ExactSolution_Init( const float Mass, const float Damp, const float Stiff, const float DeltaT, TMD_Sim *const Num );
+void ExactSolution_SDOF( const float Mass, const float Damp, const float Stif, const float u0c, const float DeltaT, TMD_Sim *const Num, float *const fc );
 
 #endif /* SUBSTRUCTURE_H */
