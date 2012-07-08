94df8c820e35e195683095c75a5876387c34948f
Ferran Obón Santacana <ferran.obon@gmail.com>
2012-07-08 11:48:03 +0200

    Added routine PrintErrorDetailAndExit()

--- Effective_Stiffness_Formulation/Common_Files/src/ErrorHandling.c
+++ Effective_Stiffness_Formulation/Common_Files/src/ErrorHandling.c
@@ -18,21 +18,30 @@
 
 void PrintErrorAndExit ( const char *ErrorMessage )
 {
-	fprintf( stderr, "%s.\n", ErrorMessage );
-	fprintf( stderr, "Exiting program." );
-	exit( EXIT_FAILURE );
+     fprintf( stderr, "%s.\n", ErrorMessage );
+     fprintf( stderr, "Exiting program." );
+     exit( EXIT_FAILURE );
 }
+
+void PrintErrorDetailAndExit( const char *ErrorMessage, const char *Detail )
+{
+     fprintf( stderr, "%s: ", ErrorMessage );
+     fprintf( stderr, "%s.\n", Detail );
+     fprintf( stderr, "Exiting program." );
+     exit( EXIT_FAILURE );
+}
+
 
 void ErrorFileAndExit( const char *ErrorMessage, const char *Filename )
 {
 
-	fprintf( stderr, "%s%s.\n", ErrorMessage, Filename );
-	fprintf( stderr, "Exiting program." );
-	exit( EXIT_FAILURE );
+     fprintf( stderr, "%s%s.\n", ErrorMessage, Filename );
+     fprintf( stderr, "Exiting program." );
+     exit( EXIT_FAILURE );
 }
 
 void LAPACKPErrorAndExit( const char *ErrorMessage1, int info, const char* ErrorMessage2 )
 {
-	fprintf( stderr, "%s%d%s.\n", ErrorMessage1, info, ErrorMessage2 );
-	exit( EXIT_FAILURE );
+     fprintf( stderr, "%s%d%s.\n", ErrorMessage1, info, ErrorMessage2 );
+     exit( EXIT_FAILURE );
 }
