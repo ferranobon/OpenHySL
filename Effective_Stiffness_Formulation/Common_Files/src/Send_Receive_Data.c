04d01cd688c48baff80b532de1576a0b5b431afd
Ferran Obón Santacana <ferran.obon@gmail.com>
2012-07-16 15:54:20 +0200

    Fixed conflicts

--- Effective_Stiffness_Formulation/Common_Files/src/Send_Receive_Data.c
+++ Effective_Stiffness_Formulation/Common_Files/src/Send_Receive_Data.c
@@ -362,7 +362,7 @@ void Send_Effective_Matrix( const float *const Eff_Mat, const int Protocol_Type,
      free( Recv );
 }
 
-void Do_Substepping( const float *const DispTdT0_c, float *const DispTdT, float *const fcprevsub, float *const fc, const int Protocol_Type, const float Time, const const int Socket, const int OrderC, const int Pos_Couple )
+void Do_Substepping( const float *const DispTdT0_c, float *const DispTdT, float *const fcprevsub, float *const fc, const int Protocol_Type, const float Time, const int Socket, const int OrderC, const int Pos_Couple )
 {
 
      int i;
