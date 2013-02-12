1c1675cc73112bbffc6527ed01dcb518741ff26d
Ferran Obón Santacana <ferran.obon@gmail.com>
2013-02-12 17:35:33 +0100

    Initial commit for the Library recoding

---
+++ Effective_Stiffness_Formulation/Library/include/EffK_Formulation.h
@@ -0,0 +1,161 @@
+/**
+ * \file EffK_Formulation.h
+ * \author Ferran Obón Santacana
+ * \version 1.0 PARDISO solver for matrix inversion is deprecated.
+ * \date 9th of February 2013
+ * 
+ * \todo Add support for packed storage to reduce memory use.
+ *
+ * \brief Rayleigh damping routines.
+ *
+ * Routines for calculating the proportional viscous damping matrix using Rayleigh Damping. The routines
+ * make use of the BLAS library to perform the linear algebra operations and they support both single and
+ * double precision. Sparse BLAS operations are supported through the Intel MKL library.
+ */
+
+#ifndef EFFK_FORMULATION_H_
+#define EFFK_FORMULATION_H_
+
+#include "MatrixVector.h"
+
+/**
+ * \brief Calculates the effective force vector according to the formulation using the effective
+ * stiffness matrix (\cite Dorka_1998).
+ * 
+ * The effective force vector \f$\vec{f_{eff}^t}\f$ is calculated according to the formulation using the effective stiffness matrix described
+ * in page 53 (\cite Dorka_1998):
+ *
+ * \f[\vec{f_{eff}^t} = \mathcal{M}(a_0\vec{u^t} + a_2\vec{\dot{u}^t} + a_3\vec{\ddot{u}^t}) + \mathcal{C}(a_1\vec{u^t} + a_4\vec{\dot{u}^t} + a_5\vec{\ddot{u}^t})\f]
+ *
+ * where:
+ * - \f$\vec{\f_{eff}^t}\f$ is the effective force vector at time \f$t\f$,
+ * - \f$\mathcal{M}\f$ is the mass matrix,
+ * - \f$\mathcal{C}\f$ is the proportional viscous damping matrix,
+ * - \f$\vec{u^t}\f$ is the displacement vector at time \f$t\f$,
+ * - \f$\vec{\dot{u}^t}\f$ is the velocity vector at time \f$t\f$,
+ * - \f$\vec{\ddot{u}^t}\f$ is the acceleration vector at time \f$t\f$,
+ * - and \f$a_0\f$, \f$a_1\f$, \f$a_2\f$, \f$a_3\f$, \f$a_4\f$ and \f$a_5\f$ are integration constants (see \cite Dorka_1998).
+ *
+ * It makes use of BLAS routines to perform the lineal algebra operations.
+ *
+ * \pre
+ * - All elements of type \c MatrixVector must be properly initialised through the Init_MatrixVector() routine.
+ * - The size of the vectors and matrices must be coherent, since it will not be checked in the routine.
+ * - The mass and viscous damping matrices must be symmetrical and only the upper part will be referenced (lower part
+ *   in FORTRAN routines).
+ * - The integration constants must be properly initialised.
+ *
+ * \param[in] Mass is the Mass matrix \f$\mathcal{M}\f$.
+ * \param[in] Damp is the Viscous Damping matrix \f$\mathcal{C}\f$.
+ * \param[in] DispT The displacement vector \f$\vec{u^t}\f$.
+ * \param[in] VelT is the velocity vector \f$\dot{\vec{u}^t}\f$.
+ * \param[in] AccT is the acceleration vector \f$\ddot{\vec{u}^t}\f$.
+ * \param[in,out] Tempvec is a temporal vector that helps in the calculations. This is included in order to avoid
+ *                allocating and deallocating memory each step. On input only the number of rows is used.
+ * \param[in] a0 The integration constant \f$a_0\f$ (\cite Dorka 1998).
+ * \param[in] a1 The integration constant \f$a_1\f$ (\cite Dorka 1998).
+ * \param[in] a2 The integration constant \f$a_2\f$ (\cite Dorka 1998).
+ * \param[in] a3 The integration constant \f$a_3\f$ (\cite Dorka 1998).
+ * \param[in] a4 The integration constant \f$a_4\f$ (\cite Dorka 1998).
+ * \param[in] a5 The integration constant \f$a_5\f$ (\cite Dorka 1998).
+ * \param[out] Eff_Force is the effective force vector \f$\vec{f_{eff}^t}\f$.
+ *
+ * \post
+ * - \c Eff_Force is the result of the operation:
+ *
+ * \f[\vec{f_{eff}^t} = \mathcal{M}(a_0\vec{u^t} + a_2\vec{\dot{u}^t} + a_3\vec{\ddot{u}^t}) + \mathcal{C}(a_1\vec{u^t} + a_4\vec{\dot{u}^t} + a_5\vec{\ddot{u}^t})\f]
+ *
+ * \sa MatrixVector.
+ */
+void EffK_EffectiveForce( const MatrixVector *const Mass, const MatrixVector *const Damp, const MatrixVector *const DispT,
+			  const MatrixVector *const VelT, const MatrixVector *const AccT, MatrixVector *const Tempvec,
+			  const double a0, const double a1, const double a2, const double a3, const double a4,
+			  const double a5, MatrixVector *const Eff_ForceT );
+
+/**
+ * \brief Computes the new acceleration according to the formulation using the effective stiffness matrix
+ * (\cite Dorka_1998).
+ *
+ * The acceleration at \f$t+\Delta t\f$ is calculated according to the formulation using the effective stiffness
+ * matrix described in page 53 (\cite Dorka_1998):
+ *
+ * \f[\vec{\ddot{u}^{t+\Delta t}} = a_0 (\vec{u}^{t+\Delta t} - \vec{u}) - a_2\vec{\dot{u}^t} -a_3\vec{\ddot{u}^{t}}\f]
+ *
+ * where:
+ * - \f$\vec{\ddot{u}^{t+\Delta t}}\f$ is the acceleration vector at time \f$t + \Delta t\f$,
+ * - \f$\vec{u^t}\f$ is the displacement vector at time \f$t\f$,
+ * - \f$\vec{u^{t+\Delta t}}\f$ is the displacement vector at time \f$t + \Delta t\f$,
+ * - \f$\vec{\dot{u}^t}\f$ is the velocity vector at time \f$t\f$,
+ * - \f$\vec{\ddot{u}^t}\f$ is the acceleration vector at time \f$t\f$,
+ * - and \f$a_0\f$, \f$a_2\f$ and \f$a_3\f$ are integration constants (see \cite Dorka_1998).
+ *
+ * It makes use of BLAS routines to perform the lineal algebra operations.
+ *
+ * \pre
+ * - All elements of type \c MatrixVector must be properly initialised through the Init_MatrixVector() routine.
+ * - The size of the vectors must be identical.
+ * - The integration constants must be properly initialised.
+ * 
+ * \param[in] DispTdT The displacement vector \f$\vec{u^{t+\Delta t}\f$.
+ * \param[in] DispT The displacement vector \f$\vec{u^t}\f$.
+ * \param[in] VelT is the velocity vector \f$\dot{\vec{u}^t}\f$.
+ * \param[in] AccT is the acceleration vector \f$\ddot{\vec{u}^t}\f$.
+ * \param[in] a0 The integration constant \f$a_0\f$ (\cite Dorka 1998).
+ * \param[in] a1 The integration constant \f$a_1\f$ (\cite Dorka 1998).
+ * \param[in] a2 The integration constant \f$a_2\f$ (\cite Dorka 1998).
+ * \param[in,out] AccTdT is the acceleration vector \f$\ddot{\vec{u}^{t+\Delta t}}\f$. On input only the number
+ *                of rows is used.
+ *
+ * \post
+ * - \c AccTdT is the result of the operation:
+ *
+ * \f[\vec{\ddot{u}^{t+\Delta t}} = a_0 (\vec{u}^{t+\Delta t} - \vec{u}) - a_2\vec{\dot{u}^t} -a_3\vec{\ddot{u}^{t}}\f]
+ *
+ * \sa MatrixVector.
+ */
+void EffK_ComputeAcceleration( const MatrixVector *const DispTdT, const MatrixVector *const DispT,
+				const MatrixVector *const VelT, const MatrixVector *const AccT,
+				const double a0, const double a2, const double a3, MatrixVector *const AccTdT );
+
+/**
+ * \brief Computes the new velocity according to the formulation using the effective stiffness matrix
+ * (\cite Dorka_1998).
+ *
+ * The velocity at \f$t+\Delta t\f$ is calculated according to the formulation using the effective stiffness matrix described
+ * in page 53 (\cite Dorka_1998):
+ *
+ * \f[\vec{\dot{u}^{t+\Delta t}} = \vec{\dot{u}^t} + a_6\vec{\ddot{u}^t} + a_7\vec{\ddot{u}^{t+\Delta t}\f]
+ *
+ * where:
+ * - \f$\vec{\dot{u}^{t+\Delta t}}\f$ is the velocity vector at time \f$t\f$,
+ * - \f$\vec{\dot{u}^t}\f$ is the velocity vector at time \f$t\f$,
+ * - \f$\vec{\ddot{u}^t}\f$ is the acceleration vector at time \f$t\f$,
+ * - \f$\vec{\ddot{u}^{t+\Delta t}}\f$ is the acceleration vector at time \f$t + \Delta t\f$,
+ * - and \f$a_6\f$ and \f$a_7\f$ are integration constants (see \cite Dorka_1998).
+ *
+ * It makes use of BLAS routines to perform the lineal algebra operations.
+ *
+ * \pre
+ * - All elements of type \c MatrixVector must be properly initialised through the Init_MatrixVector() routine.
+ * - The size of the vectors must be identical.
+ * - The integration constants must be properly initialised.
+ * 
+ * \param[in] VelT is the velocity vector \f$\dot{\vec{u}^t}\f$.
+ * \param[in] AccT is the acceleration vector \f$\ddot{\vec{u}^t}\f$.
+ * \param[in] AccTdT is the acceleration vector \f$\ddot{\vec{u}^{t+\Delta t}}\f$.
+ * \param[in] a6 The integration constant \f$a_0\f$ (\cite Dorka 1998).
+ * \param[in] a7 The integration constant \f$a_1\f$ (\cite Dorka 1998).
+ * \param[in,out] VelTdT is the velocity vector \f$\dot{\vec{u}^{t+\Delta t}}\f$. On input only the number
+ *                of rows is used.
+ *
+ * \post
+ * - \c VelTdT is the result of the operation:
+ *
+ * \f[\vec{\dot{u}^{t+\Delta t}} = \vec{\dot{u}^t} + a_6\vec{\ddot{u}^t} + a_7\vec{\ddot{u}^{t+\Delta t}\f]
+ *
+ * \sa MatrixVector.
+ */
+void EffK_ComputeVelocity( const MatrixVector *const VelT, const MatrixVector *const AccT, const MatrixVector *const AccTdT,
+			    const double a6, const double a7, MatrixVector *const VelTdT );
+
+#endif /* EFFK_FORMULATION_H_ */
---
+++ Effective_Stiffness_Formulation/Library/src/EffK_Formulation.c
@@ -0,0 +1,85 @@
+#include "EffK_Formulation.h"
+
+void EffK_EffectiveForce( const MatrixVector *const Mass, const MatrixVector *const Damp, const MatrixVector *const DispT,
+			  const MatrixVector *const VelT, const MatrixVector *const AccT, MatrixVector *const Tempvec,
+			  const double a0, const double a1, const double a2, const double a3, const double a4,
+			  const double a5, MatrixVector *const Eff_ForceT )
+{
+
+     static int incx = 1, incy = 1;  /* Stride in the vectors */
+     static char uplo = 'L';         /* The lower part (upper part in C) will be used and the upper part (lower part in C)
+				      *	will strictly not be referenced */
+     static double Alpha, Beta;      /* Constants for the BLAS routines */
+
+     /* BLAS: tempvec = Disp */
+     dcopy_( &Tempvec->Rows, DispT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: tempvec = a0*Disp = a0*tempvec */
+     Alpha = a0;
+     dscal_( &Tempvec->Rows, &Alpha, Tempvec->Array, &incx );
+     /* BLAS: tempvec = a0*Disp + a2*Vel = tempvec + a2*Vel */
+     Alpha = a2;
+     daxpy_( &Tempvec->Rows, &Alpha, VelT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: tempvec = a0*Disp + a2*Vel + a3*Acc = tempvec + a3*Acc */
+     Alpha = a3;
+     daxpy_( &Tempvec->Rows, &Alpha, AccT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: Eff_Force = Mass*(a0*Disp + a2*Vel + a3*Acc) = Mass*tempvec */
+     Alpha = 1.0; Beta = 0.0;
+     dsymv_( &uplo, &Tempvec->Rows, &Alpha, Mass->Array, &Tempvec->Rows, Tempvec->Array, &incx, &Beta,
+	     Eff_ForceT->Array, &incy );
+
+     /* BLAS: tempvec = Disp */
+     dcopy_( &Tempvec->Rows, DispT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: tempvec = a1*Disp = a0*tempvec */
+     Alpha = a1;
+     dscal_( &Tempvec->Rows, &Alpha, Tempvec->Array, &incx );
+     /* BLAS: tempvec = a1*Disp + a4*Vel = tempvec + a4*Vel */
+     Alpha = a4;
+     daxpy_( &Tempvec->Rows, &Alpha, VelT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: tempvec = a1*Disp + a4*Vel + a5*Acc = tempvec + a5*Acc */
+     Alpha = a5;
+     daxpy_( &Tempvec->Rows, &Alpha, AccT->Array, &incx, Tempvec->Array, &incy );
+     /* BLAS: Eff_Force = Mass*(a0*Disp + a2*Vel + a3*Acc) + Damp*(a1*Disp + a4*Vel + a5*Acc) = Eff_Force + Damp*tempvec */
+     Alpha = 1.0; Beta = 1.0;
+     dsymv_( &uplo, &Tempvec->Rows, &Alpha, Damp->Array, &Tempvec->Rows, Tempvec->Array, &incx, &Beta,
+	     Eff_ForceT->Array, &incy );
+     
+}
+
+void EffK_ComputeAcceleration( const MatrixVector *const DispTdT, const MatrixVector *const DispT,
+			       const MatrixVector *const VelT, const MatrixVector *const AccT,
+			       const double a0, const double a2, const double a3, MatrixVector *const AccTdT )
+{
+     static int incx = 1, incy = 1;  /* Stride in the vectors */
+     static double Alpha;            /* Constant for the BLAS routines */
+
+     /* BLAS: AccTdT = DispTdT */
+     dcopy_( &AccTdT->Rows, DispTdT->Array, &incx, AccTdT->Array, &incy ); 
+     /* BLAS: AccTdT = DispTdT - DispT = AccTdT - DispT */
+     Alpha = -1.0;
+     daxpy_( &AccTdT->Rows, &Alpha, DispT->Array, &incx, AccTdT->Array, &incy );
+     /* BLAS: AccTdT = a0*(DispTdT - DispT) = a0*AccTdT */
+     Alpha = a0;
+     dscal_( &AccTdT->Rows, &Alpha, AccTdT->Array, &incx );
+     /* BLAS: AccTdT = a0*(DispTdT - DispT) - a2*VelT = AccTdT - a2*VelT */
+     Alpha = -a2;
+     daxpy_( &AccTdT->Rows, &Alpha, VelT->Array, &incx, AccTdT->Array, &incy );
+     /* BLAS: AccTdT = a0*(DispTdT - DispT) - a2*VelT - a3*AccT = AccTdT - a3*AccT */
+     Alpha = -a3;
+     daxpy_( &AccTdT->Rows, &Alpha, AccT->Array, &incx, AccTdT->Array, &incy );
+}
+
+void EffK_ComputeVelocity( const MatrixVector *const VelT, const MatrixVector *const AccT, const MatrixVector *const AccTdT,
+			   const double a6, const double a7, MatrixVector *const VelTdT )
+{
+     static int incx = 1, incy= 1;  /* Stride in the vectors */
+     static double Alpha;           /* Constant for the BLAS routines */
+
+     /* BLAS: VelTdT = VelT */
+     dcopy_( &VelTdT->Rows, VelT->Array, &incx, VelTdT->Array, &incy );
+     /* BLAS: VelTdT = VelT + a6*AccT = VelTdT + a6*AccT */
+     Alpha = a6;
+     daxpy_( &VelTdT->Rows, &Alpha, AccT->Array, &incx, VelTdT->Array, &incy );
+     /* BLAS: VelTdT = VelT + a6*AccT + a7*AccTdT = VelTdT + a7*AccTdT */
+     Alpha = a7;
+     daxpy_( &VelTdT->Rows, &Alpha, AccTdT->Array, &incx, VelTdT->Array, &incy );
+}
