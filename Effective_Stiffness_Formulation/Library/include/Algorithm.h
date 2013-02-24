a07b88a4bfecc0ceae00526a0c0d5af9bda94a50
Ferran Obón Santacana <ferran.obon@gmail.com>
2013-02-24 12:35:48 +0100

    Removed duplicated routines. This file will contain, from now on, the routines that are dependant on the test

---
+++ Effective_Stiffness_Formulation/Library/include/Algorithm.h
@@ -0,0 +1,7 @@
+#ifndef _ALGORITHM_H_
+#define _ALGORITHM_H_
+
+void Init_Algorithm( AlgConst *const InitConst, const char *FileName );
+void Delete_Algorithm( AlgConst *const InitConst );
+
+#endif _ALGORITHM_H_
