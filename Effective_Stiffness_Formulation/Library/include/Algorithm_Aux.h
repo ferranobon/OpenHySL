2704eba2d2d436fd6deeff7e5401967db7a46c08
Ferran Obón Santacana <ferran.obon@uni-kassel.de>
2013-03-01 22:57:20 +0100

    Added routine Get_Excited_DOF() with doxygen documentation. Formated the files.

--- Effective_Stiffness_Formulation/Library/include/Algorithm_Aux.h
+++ Effective_Stiffness_Formulation/Library/include/Algorithm_Aux.h
@@ -2,7 +2,8 @@
 #define _ALGORITHM_AUX_H_
 
 #include "Initiation.h"         /* For Rayleigh_t */
-#include "Error_Compensation.h"  /* For PID_t */
+#include "Error_Compensation.h" /* For PID_t */
+#include "Conf_Parser.h"        /* For ConfFile_t */
 
 /**
  * \brief Structure to handle the time integration constants.
@@ -18,37 +19,48 @@ typedef struct TIntegration{
 /**
  * \brief Structure to wrap constants and filenames.
  *
- * This structure stores several constants that will be used in different parts of the substructure
- * algorithm, such as the order of the matrices, the position of the coupling nodes, number of steps
- * filenames and other contants. It act as a configuration file and it has to be modified by hand
- * before compiling.
+ * This structure stores several constants that will be used in different parts of the substructure algorithm, such as
+ * the order of the matrices, the position of the coupling nodes, number of steps filenames and other contants. It act
+ * as a configuration file and it has to be modified by hand before compiling.
  *
  * \sa Rayleigh_t, Newmark_t, PID_t and Algorithm_Init().
- *
  */
 typedef struct AlgConst{
 
      int Order;               /*!< \brief Order of the matrices */
-     int OrderSub;   /*!< \brief Number of substructures */
+     int OrderSub;            /*!< \brief Number of substructures */
 
      unsigned int Nstep;      /*!< \brief Number of steps */
      unsigned int NSubstep;   /*!< \brief Number of sub-steps */
 
-     int Use_Absolute_Values; /*!< \brief Variable to control whether to use absolute values in the equation of motion or relative values. Affects how the input load is calculated */
+     int Use_Absolute_Values; /*!< \brief Variable to control whether to use absolute values in the equation of motion
+			       * or relative values. Affects how the input load is calculated
+			       */
 
-     int Read_Sparse;         /*!< \brief The matrices will be read using a sparse format. \sa MatrixVector_From_File_Sp2Dense() and MatrixVector_From_File_Sp(). */
-     int Use_Sparse;          /*!< \brief Sparse routines will be used instead of dense ones. Only available with the support of the Intel MKL libraries.
-				\sa MatrixVector_From_File_Sp(), CalculateMatrixC_Sp(), CalculateMatrixKeinv_Pardiso_Sparse(), Calc_Input_Load_AbsValues_Sparse(),
-				Calc_Input_Load_RelValues_Sparse() and EffK_Calc_Effective_Force_Sparse(). */
+     int Read_Sparse;         /*!< \brief The matrices will be read using a sparse format. \sa
+			       * MatrixVector_From_File_Sp2Dense() and MatrixVector_From_File_Sp().
+			       */
+     int Use_Sparse;          /*!< \brief Sparse routines will be used instead of dense ones. Only available with the
+			       * support of the Intel MKL libraries.
+			       * \sa MatrixVector_From_File_Sp(), CalculateMatrixC_Sp(),
+			       * CalculateMatrixKeinv_Pardiso_Sparse(), Calc_Input_Load_AbsValues_Sparse(),
+			       * Calc_Input_Load_RelValues_Sparse() and EffK_Calc_Effective_Force_Sparse().
+			       */
      int Read_LVector;        /*!< \brief Read the load vector instead of generating it. */
      int *ExcitedDOF;
 
-     double Delta_t;           /*!< \brief Time increment \f$\Delta t\f$ */
-     double DeltaT_Sub;        /*!< \brief Time increment for the sub-stepping process */
+     double Delta_t;          /*!< \brief Time increment \f$\Delta t\f$ */
+     double DeltaT_Sub;       /*!< \brief Time increment for the sub-stepping process */
 
-     Rayleigh_t Rayleigh;  /*!< \brief Stores Rayleigh Constants alpha (\c Rayleigh.Alpha or \f$\alpha_R\f$) and beta (\c Rayleigh.Beta or \f$\beta_R\f$) */
-     TIntegration_t Newmark;    /*!< \brief Stores Newmark Constants gamma (\c Newmark.Gamma or \f$\gamma_N\f$) and (\c Newmark.Beta or \f$\beta_N\f$) */
-     PID_t PID;           /*!< \brief Stores proportional (\c PID.P), integral (\c PID.I) and derivative (\c PID.D) part. */
+     Rayleigh_t Rayleigh;     /*!< \brief Stores Rayleigh Constants alpha (\c Rayleigh.Alpha or \f$\alpha_R\f$) and beta
+			       * (\c Rayleigh.Beta or \f$\beta_R\f$)
+			       */
+     TIntegration_t Newmark;  /*!< \brief Stores Newmark Constants gamma (\c Newmark.Gamma or \f$\gamma_N\f$) and (\c
+			       * Newmark.Beta or \f$\beta_N\f$)
+			       */
+     PID_t PID;               /*!< \brief Stores proportional (\c PID.P), integral (\c PID.I) and derivative (\c PID.D)
+			       * part.
+			       */
 
      /* Constants for Calculations */
      double Const1;           /*!< \brief \f$Const_1=\beta_N\Delta t^2\f$ */
@@ -69,9 +81,10 @@ typedef struct AlgConst{
      char* FileM;            /*!< \brief Stores the name of the file that contains the Mass Matrix */
      char* FileK;            /*!< \brief Stores the name of the file that contains the Stiffness Matrix */
      char* FileC;            /*!< \brief Stores the name of the file that contains the Damping Matrix */
-     char* FileLV;            /*!< \brief Stores the name of the file that contains the Load Vector */
+     char* FileLV;           /*!< \brief Stores the name of the file that contains the Load Vector */
      char* FileCNodes;       /*!< \brief Stores the name of the file that contains the vector of coupling nodes. */
-     char* FileData;         /*!< \brief Stores the name of the file that contains displacement, velocity and acceleration */
+     char* FileData;         /*!< \brief Stores the name of the file that contains displacement, velocity and
+			      * acceleration */
      char* FileOutput;       /*!< \brief Name of the file to store the output values of the process */
 } AlgConst_t;
 
@@ -79,22 +92,23 @@ typedef struct AlgConst{
 /**
  * \brief Definition of constant values and filenames that will be used during the Algorithm.
  *
- * This routine reads the values specified in a configuration file. In it, several constants like the order of the matrices,
- * the number of steps and file names where the Mass and Stiffness matrices are stored (the damping matrix is optional).
+ * This routine reads the values specified in a configuration file. In it, several constants like the order of the
+ * matrices, the number of steps and file names where the Mass and Stiffness matrices are stored (the damping matrix is
+ * optional).
  *
  *
- * \pre The values of the Newmark integration, PID and Rayleigh constants must be coherent/feasible. The algorithm will not perform checks on them.
+ * \pre The values of the Newmark integration, PID and Rayleigh constants must be coherent/feasible. The algorithm will
+ * not perform checks on them.
  *
  * \param[in] FileName Name of the configuration file.
  * \param[out] InitConst A structure that comprises of several constants.
  *
  * \post
- * - The size of the matrices will determine the memory that will be allocated when defining a \c MatrixVector_t type and also how
- * many elements will be read/written from/to the files.
- * - The number of steps must be equal to the number of rows of the file "DataFile".
-
- * \sa AlgoConst_t, PID_t, Rayleigh_t and TIntegration_t.
+ * - The size of the matrices will determine the memory that will be allocated when defining a \c MatrixVector_t type
+ * and also also how many elements will be read/written from/to the files.
+ *- The number of steps must be equal to the number of rows of the file "DataFile".
  *
+ * \sa AlgoConst_t, PID_t, Rayleigh_t and TIntegration_t.
  */
 void Algorithm_Init( const char *FileName, AlgConst_t *const InitConst );
 
@@ -108,5 +122,39 @@ void Algorithm_Init( const char *FileName, AlgConst_t *const InitConst );
  * \sa AlgConst_t.
  */
 void Algorithm_Destroy( AlgConst_t *const InitConst );
+
+/**
+ * \brief Transforms a string with the desired DOFs to be excited into an integer array.
+ *
+ * \pre
+ * - The first element of the list should contain the number of DOFs per node.
+ * - The next \f$n\f$ entries are the degrees of freedom that are being excited (1) or not (0) by the external load.
+ * - The order of which DOFs will have an external load applied must remain constant and keep the same patern.
+ * - The rest of \f$\frac{Order}{n}\f$ must be equal to 0, where \f$Order\f$ is the length of the external load vector.
+ * - \c Config must be properly initialised through ConfFile_Create() and have entries stored through ConfFile_ReadFile().
+ * - \c Expression must be a valid entry in \c Config.
+ *
+ * Transforms a string with the desired sequence of DOFs that will have an external load applied. The string consist of
+ * a first element that determines how many values will follow, \f$n\f$. The next \f$n\f$ values are the pattern and
+ * they identify if a degree of freedom is excited (1) or not (0). The pattern will be applied as many times as \f$N =
+ * \frac{Order}{n}\f$, where \f$Order\f$ is the length of the external load vector.
+ *
+ * \b Example \b 1:
+ *
+ * <tt>6 1 1 1 0 0 0 </tt>
+ *
+ * The first three elements of the external load vector will have a value \f$u_g*1.0\f$ while the other three will be
+ * set to 0.0. The pattern will repeat for every 6 elements in the load vector.
+ *
+ * \b Example \b 2:
+ *
+ * <tt>4 1 0 1 1</tt>
+ *
+ * Only the second element of every four positions in the external load vector will be set to 0.0.
+ *
+ * \param[in] Config Structure containing the entries of the configuration file.
+ * \param[in] Expression String with the desired DOFs to be excited.
+ */
+int* Get_Excited_DOF( const ConfFile_t *const Config, const char *Expression );
 
 #endif /* _ALGORITHM_AUX_H_ */
--- Effective_Stiffness_Formulation/Library/src/Algorithm_Aux.c
+++ Effective_Stiffness_Formulation/Library/src/Algorithm_Aux.c
@@ -2,95 +2,183 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "Algorithm.h"
+#include "Algorithm_Aux.h"
+#include "Conf_Parser.h"
+#include "Print_Messages.h"
 
-void Init_Algorithm( AlgConst *const InitConst, const char *FileName )
+
+void Algorithm_Init( const char *FileName, AlgConst_t *const InitConst )
 {
 
-     ConfFile *Config;
+     ConfFile_t *Config;
      
-     Config = ConfFile_Create( 35 );
+     Config = ConfFile_Create( 70 );
 
-     ConfFile_ReadFile( Config, FileName );
+     ConfFile_ReadFile( FileName, Config );
 
      /* Use Relative or absolute values */
      (*InitConst).Use_Absolute_Values = ConfFile_GetInt( Config, "General:Use_Absolute_Values" );
      if ( InitConst->Use_Absolute_Values != 0 && InitConst->Use_Absolute_Values != 1 ){
-	  PrintErrorAndExit( "Invalid option for Use_Absolute_Values" );
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for Use_Absolute_Values.\n" );
+	  exit( EXIT_FAILURE );
+     }
+
+     InitConst->Read_Sparse = ConfFile_GetInt( Config, "General:Read_Sparse" );
+     if ( InitConst->Read_Sparse != 0 && InitConst->Read_Sparse != 1 ){
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for Read_Sparse.\n" );
+     }
+
+     InitConst->Use_Sparse = ConfFile_GetInt( Config, "General:Use_Sparse" );
+     if ( InitConst->Use_Sparse != 0 && InitConst->Use_Sparse != 1 ){
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for Use_Sparse.\n" );
+     }
+
+     InitConst->Read_LVector = ConfFile_GetInt( Config, "General:Read_LVector" );
+     if ( InitConst->Read_LVector != 0 && InitConst->Read_LVector != 1 ){
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for Read_LVector.\n" );
      }
 
      /* Order of the matrices */
      (*InitConst).Order = ConfFile_GetInt( Config, "General:Order" );
      if ( InitConst->Order <= 0 ){
-	  PrintErrorAndExit( "Invalid option for the order of the matrices" );
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for the order of the matrices.\n" );
+	  exit( EXIT_FAILURE );
+     }
+
+     if( !InitConst->Read_LVector ){
+	  InitConst->ExcitedDOF = Get_Excited_DOF( Config, "General:Excited_DOF" );
+     } else {
+	  InitConst->ExcitedDOF = NULL;
      }
 
      /* Number of steps and Time step */
      (*InitConst).Nstep = (unsigned int) ConfFile_GetInt( Config, "General:Num_Steps" );
      if ( InitConst->Nstep <= 0 ){
-	  PrintErrorAndExit( "Invalid number of steps" );
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid number of steps.\n" );
+	  exit( EXIT_FAILURE );
      }
 
-     (*InitConst).Delta_t = ConfFile_GetFloat( Config, "General:Delta" );
-     if ( InitConst->Delta_t <= 0.0f ){
-	  PrintErrorAndExit( "Invalid time step" );
+     (*InitConst).Delta_t = ConfFile_GetDouble( Config, "General:Delta" );
+     if ( InitConst->Delta_t <= 0.0 ){
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid time step.\n" );
+	  exit( EXIT_FAILURE );
      }
 
      /* Rayleigh values */
-     (*InitConst).Rayleigh.Alpha = ConfFile_GetFloat( Config, "Rayleigh:Alpha" );
-     (*InitConst).Rayleigh.Beta = ConfFile_GetFloat( Config, "Rayleigh:Beta" );
+     (*InitConst).Rayleigh.Alpha = ConfFile_GetDouble( Config, "Rayleigh:Alpha" );
+     (*InitConst).Rayleigh.Beta = ConfFile_GetDouble( Config, "Rayleigh:Beta" );
 
      /* Newmark integration constants */
-     (*InitConst).Newmark.Gamma = ConfFile_GetFloat( Config, "Newmark:Gamma" );
-     (*InitConst).Newmark.Beta = ConfFile_GetFloat( Config, "Newmark:Beta" );
+     (*InitConst).Newmark.Gamma = ConfFile_GetDouble( Config, "Newmark:Gamma" );
+     (*InitConst).Newmark.Beta = ConfFile_GetDouble( Config, "Newmark:Beta" );
 
      /* PID Constants */
-     (*InitConst).PID.P = ConfFile_GetFloat( Config, "PID:P" );
-     (*InitConst).PID.I = ConfFile_GetFloat( Config, "PID:I" );
-     (*InitConst).PID.D = ConfFile_GetFloat( Config, "PID:D" );
+     (*InitConst).PID.P = ConfFile_GetDouble( Config, "PID:P" );
+     (*InitConst).PID.I = ConfFile_GetDouble( Config, "PID:I" );
+     (*InitConst).PID.D = ConfFile_GetDouble( Config, "PID:D" );
 
      /* Several constants to multiply the vectors */
      (*InitConst).Const1 = (*InitConst).Newmark.Beta*(*InitConst).Delta_t*(*InitConst).Delta_t;
-     (*InitConst).Const2 = (0.5f - 2.0f*(*InitConst).Newmark.Beta + (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t*(*InitConst).Delta_t;
-     (*InitConst).Const3 = (0.5f + (*InitConst).Newmark.Beta - (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t*(*InitConst).Delta_t;
+     (*InitConst).Const2 = (0.5 - 2.0*(*InitConst).Newmark.Beta + (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t*(*InitConst).Delta_t;
+     (*InitConst).Const3 = (0.5 + (*InitConst).Newmark.Beta - (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t*(*InitConst).Delta_t;
 
      /* Constants for Ending Step */
-     (*InitConst).a0 = 1.0f/((*InitConst).Newmark.Beta*(*InitConst).Delta_t*(*InitConst).Delta_t);
+     (*InitConst).a0 = 1.0/((*InitConst).Newmark.Beta*(*InitConst).Delta_t*(*InitConst).Delta_t);
      (*InitConst).a1 = (*InitConst).Newmark.Gamma/((*InitConst).Newmark.Beta*(*InitConst).Delta_t);
-     (*InitConst).a2 = 1.0f/((*InitConst).Newmark.Beta*(*InitConst).Delta_t);
-     (*InitConst).a3 = 1.0f/(2.0f*(*InitConst).Newmark.Beta) - 1.0f;
-     (*InitConst).a4 = (*InitConst).Newmark.Gamma/(*InitConst).Newmark.Beta - 1.0f;
-     (*InitConst).a5 = ((*InitConst).Delta_t/2.0f)*((*InitConst).Newmark.Gamma/(*InitConst).Newmark.Beta - 2.0f);
-     (*InitConst).a6 = (1.0f - (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t;
+     (*InitConst).a2 = 1.0/((*InitConst).Newmark.Beta*(*InitConst).Delta_t);
+     (*InitConst).a3 = 1.0/(2.0*(*InitConst).Newmark.Beta) - 1.0;
+     (*InitConst).a4 = (*InitConst).Newmark.Gamma/(*InitConst).Newmark.Beta - 1.0;
+     (*InitConst).a5 = ((*InitConst).Delta_t/2.0)*((*InitConst).Newmark.Gamma/(*InitConst).Newmark.Beta - 2.0);
+     (*InitConst).a6 = (1.0 - (*InitConst).Newmark.Gamma)*(*InitConst).Delta_t;
      (*InitConst).a7 = (*InitConst).Newmark.Gamma*(*InitConst).Delta_t;
 
      /* File Names */
-/*EFAST*/
      (*InitConst).FileM = strdup( ConfFile_GetString( Config, "FileNames:Mass_Matrix" ) );
      (*InitConst).FileK = strdup( ConfFile_GetString( Config, "FileNames:Stiffness_Matrix" ) );
      (*InitConst).FileC = strdup( ConfFile_GetString( Config, "FileNames:Damping_Matrix" ) );
-     (*InitConst).FileLVector = strdup( ConfFile_GetString( Config, "FileNames:FileLVector" ) );
+     if( InitConst->Read_LVector ){
+	  (*InitConst).FileLV = strdup( ConfFile_GetString( Config, "FileNames:Load_Vector" ) );
+
+     } else {
+	  InitConst->FileLV = NULL;
+     }
      (*InitConst).FileCNodes = strdup( ConfFile_GetString( Config, "FileNames:Coupling_Nodes" ) );
      (*InitConst).FileData = strdup( ConfFile_GetString( Config, "FileNames:Ground_Motion" ) );
      (*InitConst).FileOutput = strdup( ConfFile_GetString( Config, "FileNames:OutputFile" ) );
 
-     /* Get the communication protocol to be used */
-     GetServerInformation( &InitConst->Remote, Config );
-     ConfFile_Free( Config );
+     /* Read the information regarding the numerical sub-structures */
+
+     /* Number of substructures */
+     (*InitConst).OrderSub = ConfFile_GetInt( Config, "Substructure:Order" );
+     if ( InitConst->OrderSub < 0 ){
+	  Print_Header( ERROR );
+	  fprintf( stderr, "Invalid option for the number of sub-structuresr of the matrices.\n" );
+	  exit( EXIT_FAILURE );
+     }
+     
+     /* Number of substructures */
+     InitConst->NSubstep = (unsigned int) ConfFile_GetInt( Config, "Substructure:Num_Substeps" );
+
+     InitConst->DeltaT_Sub = InitConst->Delta_t/(double) InitConst->NSubstep;
+
+     ConfFile_Destroy( Config );
+
+     Print_Header( SUCCESS );
+     printf( "Initialisation succcessfully completed.\n" );
 }
 
-void Delete_Algorithm( AlgConst *const InitConst )
+void Algorithm_Destroy( AlgConst_t *const InitConst )
 {
 
+    if( InitConst->ExcitedDOF != NULL ){
+	  free( InitConst->ExcitedDOF );
+     }
      free( InitConst->FileM );
      free( InitConst->FileK );
+
      if( InitConst->FileC != NULL ){
 	  free( InitConst->FileC );
      }
-     free( InitConst->FileLVector );
+     if( InitConst->FileLV != NULL ){
+	  free( InitConst->FileLV );
+     }
      free( InitConst->FileCNodes );
      free( InitConst->FileData );
      free( InitConst->FileOutput );
+}
 
-     Delete_ServerInformation( &InitConst->Remote );
+int* Get_Excited_DOF( const ConfFile_t *const Config, const char *Expression )
+{
+     unsigned int i, j;
+     int *DOF_Table;  
+     char *FullString;
+     char Temp[1];
+
+     FullString = strdup( ConfFile_GetString( Config, Expression ) );
+
+     /* The first position contains the number of degrees of Freedom per node present in the
+      * structure */
+     strncpy( Temp, &FullString[0], (size_t) 1 );
+
+     DOF_Table = (int *) calloc( (size_t) (atoi(Temp)+1), sizeof(int) );
+     DOF_Table[0] = atoi( Temp );
+
+     j = 1;
+     for( i = 1; i < strlen( FullString ); i++ ){
+	  if ( FullString[i] != ' ' ){
+	       strncpy( Temp, &FullString[i], (size_t) 1 );
+	       DOF_Table[j] = atoi( Temp );
+	       j = j + 1;
+	  }
+     }
+
+     free( FullString );
+     return DOF_Table;
 }
