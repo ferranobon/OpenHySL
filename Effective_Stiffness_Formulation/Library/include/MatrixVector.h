754e2af01709350db7690dde504f01e9fb1a26ac
Ferran Obón Santacana <ferran.obon@gmail.com>
2013-03-20 17:38:44 +0100

    Code formating.

--- Effective_Stiffness_Formulation/Library/include/MatrixVector.h
+++ Effective_Stiffness_Formulation/Library/include/MatrixVector.h
@@ -6,7 +6,8 @@
  *
  * \brief MatrixVector_t creation and manipulation prototypes.
  *
- * This file contains the prototypes of those functions involved in creating/destroying dense matrices and vectors.
+ * This file contains the prototypes of those functions involved in creating/destroying dense matrices and
+ * vectors.
  */
 
 #ifndef MATRIXVECTOR_H_
@@ -15,9 +16,10 @@
 /**
  * \brief General matrix storage.
  *
- * Structure designed to represent matrix and vector types within the library. It stores the number of rows and columns
- * as well as the values within the matrix. It uses a 1-dimensional array for this purpose. This is intended to be used
- * only for dense matrices and vectors. If a sparse format is desired, \c MatrixVector_Sp should be used instead. 
+ * Structure designed to represent matrix and vector types within the library. It stores the number of rows
+ * and columns as well as the values within the matrix. It uses a 1-dimensional array for this purpose. This
+ * is intended to be used only for dense matrices and vectors. If a sparse format is desired, \c
+ * MatrixVector_Sp should be used instead.
  *
  * \sa MatrixVector_Sp_t.
  */
@@ -43,36 +45,34 @@ typedef struct Scal{
  * \warning The effect of using this routine on vectors is unknown.
  *
  * \pre
- * - All elements of type \c MatVec must be properly initialised through
- * MatrixVector_Create().
- * - The matrices must be symmetrical and only the upper part will be referenced (lower
- * part in FORTRAN routines).
- * - \f$S(\mathcal Y) \geq max(S(\mathcal A),S(\mathcal B),S(\mathcal C))\f$ where
- *   \f$S(\mathcal X) = X.Rows*X.Cols\f$ is the size of the matrix.
+ * - All elements of type \c MatVec must be properly initialised through MatrixVector_Create().
+ * - The matrices must be symmetrical and only the upper part will be referenced (lower part in FORTRAN
+ *   routines).
+ * - \f$S(\mathcal Y) \geq max(S(\mathcal A),S(\mathcal B),S(\mathcal C))\f$ where \f$S(\mathcal X) =
+ *   X.Rows*X.Cols\f$ is the size of the matrix.
  *
  * This routine adds three matrices through the operation:
  *
  * \f[\mathcal Y = \alpha \mathcal A + \beta \mathcal B + \gamma \mathcal C\f]
  *
  * where:
- * - \f$\mathcal Y\f$, \f$\mathcal A\f$, \f$\mathcal B\f$, \f$\mathcal C\f$ are symmetric
- * matrices,
+ * - \f$\mathcal Y\f$, \f$\mathcal A\f$, \f$\mathcal B\f$, \f$\mathcal C\f$ are symmetric matrices,
  * - and \f$\alpha\f$, \f$\beta\f$, \f$\gamma\f$ are real scalars.
  *
- * It makes use of BLAS and LAPACK routines to perform the lineal algebra operations. For
- * the packed storage and sparse version use the MatrixVector_Add3Mat_PS() and
- * MatrixVector_Add3Mat_Sp() routines respectively.
+ * It makes use of BLAS and LAPACK routines to perform the lineal algebra operations. For the packed storage
+ * and sparse version use the MatrixVector_Add3Mat_PS() and MatrixVector_Add3Mat_Sp() routines respectively.
  * 
- * \param[in] MatA Symmetric matrix \f$\mathcal A\f$ with only the upper part referenced
- * (lower part in FORTRAN routines).
- * \param[in] MatB Symmetric matrix \f$\mathcal B\f$ with only the upper part referenced
- * (lower part in FORTRAN routines).
- * \param[in] MatC Symmetric matrix \f$\mathcal C\f$ with only the upper part referenced
- * (lower part in FORTRAN routines).
- * \param[in] Const Scalars that multiply the matrices A (\f$\alpha\f$), \c Const.Aplha),
- * B (\f$\beta\f$), \c Const.Beta) and C (\f$\gamma\f$), \c Const.Gamma).
- * \param[in] MatY Symmetric matrix \f$\mathcal Y\f$ with only the upper part referenced
- * (lower part in FORTRAN routines). On entry only the dimensions are referenced.
+ * \param[in]     MatA  Symmetric matrix \f$\mathcal A\f$ in general storage with only the upper part
+ *                      referenced (lower part in FORTRAN routines).
+ * \param[in]     MatB  Symmetric matrix \f$\mathcal B\f$ in general storage with only the upper part
+ *                      referenced (lower part in FORTRAN routines).
+ * \param[in]     MatC  Symmetric matrix \f$\mathcal C\f$ in general storage with only the upper part
+ *                      referenced (lower part in FORTRAN routines).
+ * \param[in]     Const Scalars that multiply the matrices A (\f$\alpha\f$), \c Const.Aplha), B (\f$\beta\f$),
+ *                      \c Const.Beta) and C (\f$\gamma\f$), \c Const.Gamma).
+ * \param[in,out] MatY  Symmetric matrix \f$\mathcal Y\f$ in general storage with only the upper part
+ *                      referenced (lower part in FORTRAN routines). On entry only the dimensions are
+ *                      referenced.
  *
  * \post \c MatY is the result of the operation:
  * \f[\mathcal Y = \alpha \mathcal A + \beta \mathcal B + \gamma \mathcal C\f]
@@ -86,19 +86,19 @@ void MatrixVector_Add3Mat( const MatrixVector_t *const MatA, const MatrixVector_
  *
  * \pre \f$ Rows \geq 0\f$ and \f$Cols \geq 0 \f$.
  *
- * A \c MatrixVector_t type is initialised. The routine allocates an amount of memory as a
- * single dimenson array with length \f$L = Rows*Cols\f$. The number of rows and columns
- * is also stored and all elements of the array are initialised and set to 0.0. For
- * symmetric matrices in packed storage or sparse matrices the routines
- * MatrixVector_Create_PS() or MatrixVector_Create_Sp() should be used respectively.
+ * A \c MatrixVector_t type is initialised. The routine allocates an amount of memory as a single dimenson
+ * array with length \f$L = Rows*Cols\f$. The number of rows and columns is also stored and all elements of
+ * the array are initialised and set to 0.0. For symmetric matrices in packed storage or sparse matrices the
+ * routines MatrixVector_Create_PS() or MatrixVector_Create_Sp() should be used respectively.
  *
- * \param[in] Rows The number of rows.
- * \param[in] Cols The number of columns.
+ * \param[in]  Rows   The number of rows.
+ * \param[in]  Cols   The number of columns.
  * \param[out] MatVec The matrix or vector to initialise.
  *
  * \post
  * - <tt>MatVec.Rows = Rows</tt> and <tt>MatVec.Cols = Cols</tt>.
- * - The length of the allocated double array is set to \f$L = Rows*Cols\f$ and all its values initialised to 0.0.
+ * - The length of the allocated double array is set to \f$L = Rows*Cols\f$ and all its values initialised to
+ *   0.0.
  * - The memory should be deallocated through MatrixVector_Destroy().
  *
  * \sa MatrixVector_t.
@@ -125,8 +125,8 @@ void MatrixVector_Destroy( MatrixVector_t *const MatVec );
  * 
  * \pre \c MatVec must be properly initialised through MatrixVector_Create().
  *
- * \param[in] Filename Name of the ASCII file to be opened.
- * \param[in,out] MatVec On input only the number of rows and columns is referenced.
+ * \param[in]     Filename Name of the ASCII file to be opened.
+ * \param[in,out] MatVec   On input only the number of rows and columns is referenced.
  *
  * \post \c MatVec.Array have the contents of the ASCII file.
  */
@@ -141,13 +141,13 @@ void MatrixVector_FromFile( const char *Filename, MatrixVector_t *const MatVec )
  * - \c MatVec must be properly initialised through MatrixVector_Create().
  * - \c Filename must be in MatrixMarket format and stored in a sparse way.
  *
- * This routine reads a matrix or a vector from a MatrixMarket (\cite MatrixMarket)
- * formatted file. It can handle only sparse formats but the output will always be a dense
- * matrix. If a sparse or a packed storage format are desired the routines
- * MatrixVector_FromFile_MM_Sp() or MatrixVector_FromFile_MM_PS() should be used instead.
+ * This routine reads a matrix or a vector from a MatrixMarket (\cite MatrixMarket) formatted file. It can
+ * handle only sparse formats but the output will always be a dense matrix. If a sparse or a packed storage
+ * format are desired the routines MatrixVector_FromFile_MM_Sp() or MatrixVector_FromFile_MM_PS() should be
+ * used instead.
  *
- * \param[in] Filename The file with a MatrixMarket format.
- * \param[in,out] MatVec On input only the number of rows and columns is referenced.
+ * \param[in]     Filename The file with a MatrixMarket format.
+ * \param[in,out] MatVec   On input only the number of rows and columns is referenced.
  *
  * \post \c MatVec.Array have the contents of the file always in dense storage.
  */
@@ -161,9 +161,8 @@ void MatrixVector_FromFile_MM( const char *Filename, MatrixVector_t *const MatVe
  * - RowIndex and ColIndex must be in one based index.
  * - \c Operation must be \c Set, \c Add, \c Multiply or \c Divide.
  * 
- * A basic linear algebra operation is performed on one of the elements of the matrix or
- * vector. The operation performed is controlled through the variable \c
- * Operation. Currently only four operations are supported.
+ * A basic linear algebra operation is performed on one of the elements of the matrix or vector. The operation
+ * performed is controlled through the variable \c Operation. Currently only four operations are supported.
  * 
  * - <tt>Operation = Set</tt>. \f$A(i,j) = \alpha\f$.
  * - <tt>Operation = Add</tt>. \f$A(i,j) = A(i,j) + \alpha\f$.
@@ -172,18 +171,16 @@ void MatrixVector_FromFile_MM( const char *Filename, MatrixVector_t *const MatVe
  *
  * If the operation is not supported, the routine calls <tt>exit( EXIT_FAILURE )</tt>.
  *
- * \param[in] RowIndex The row index \f$i\f$ (one based index).
- * \param[in] ColIndex The column index \f$j\f$ (one based index).
- * \param[in] Alpha The value to be set, added, multiplied and divided \f$\alpha\f$.
- * \param[in] Alpha The value to be set, added, multiplied and divided \f$\alpha\f$.
- * \param[in] Operation Controls what operation is performed: \c Set, \c Add, \c Multiply
- * or \c Divide.
- * \param[in,out] MatVec Matrix or vector to be modified. On entry only the number of
- * columns and the value (except in the case when <tt>Operation = Set</tt>) are
- * referenced. On output \f$A(i,j)\f$, is modified accordingly.
+ * \param[in]     RowIndex  The row index \f$i\f$ (one based index).
+ * \param[in]     ColIndex  The column index \f$j\f$ (one based index).
+ * \param[in]     Alpha     The value to be set, added, multiplied and divided \f$\alpha\f$.
+ * \param[in]     Operation Controls what operation is performed: \c Set, \c Add, \c Multiply or \c Divide.
+ * \param[in,out] MatVec    Matrix or vector to be modified. On entry only the number of columns and the value
+ *                          (except in the case when <tt>Operation = Set</tt>) are referenced. On output
+ *                          \f$A(i,j)\f$, is modified accordingly.
  *
- * \post One of the supported operations is performed. If the operation is not supported,
- * the routine calls <tt>exit( EXIT_FAILURE )</tt>.
+ * \post One of the supported operations is performed. If the operation is not supported, the routine calls
+ * <tt>exit( EXIT_FAILURE )</tt>.
  */
 void MatrixVector_ModifyElement( const int RowIndex, const int ColIndex, const double Alpha,
 				 const char *Operation, MatrixVector_t *const MatVec );
@@ -193,13 +190,13 @@ void MatrixVector_ModifyElement( const int RowIndex, const int ColIndex, const d
  *
  * \pre \c MatVec must be properly initialised through MatrixVector_Create().
  *
- * All the elements in \c MatVec.Array are set to the specified value. For a packed
- * storage representation of the matrix the routine MatrixVector_Set2Value() should be
- * used instead. It makes use of BLAS routines to perform the lineal algebra operations.
+ * All the elements in \c MatVec.Array are set to the specified value. For a packed storage representation of
+ * the matrix the routine MatrixVector_Set2Value() should be used instead. It makes use of BLAS routines to
+ * perform the lineal algebra operations.
  *
- * \param[in] Value All elements of the matrix or vector will be set to this value.
- * \param[in,out] MatVec Matrix or vector. On input only the number of rows and columns is
- * referenced. On output all its elements are set to \c Value.
+ * \param[in]     Value  All elements of the matrix or vector will be set to this value.
+ * \param[in,out] MatVec Matrix or vector. On input only the number of rows and columns is referenced. On
+ *                       output all its elements are set to \c Value.
  *
  * \post All the elements in \c MatVec.Array are set to \c Value.
  */
@@ -210,7 +207,7 @@ void MatrixVector_Set2Value( const double Value, MatrixVector_t *const MatVec );
  * 
  * \pre \c MatVec must be properly initialised through MatrixVector_Create().
  *
- * \param[in] MatVec The matrix or vector to save to the file \c Filename.
+ * \param[in] MatVec   The matrix or vector to save to the file \c Filename.
  * \param[in] Filename Name of the ASCII file to be opened.
  *
  * \post The ASCII file \c Filename has the contents of \c MatVec.Array in a dense format.
